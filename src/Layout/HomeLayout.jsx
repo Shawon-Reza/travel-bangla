93952d9d0f1cc6af1dbcf252c9ae697e344100d7
Shawon Reza <shawon.reza.sr81@gmail.com>
2025-05-08 14:13:47 +0600

    animated partical bg add on home page using ReactBits library

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,23 +1,35 @@
-import Background from "../Components/Background";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import Navbar from "../Components/Navbar";
+import Particles from '../Animated/particles';
+
+
 
 const HomeLayout = () => {
-
     return (
-        <div className="">
-
-            <Background></Background>
-            <Navbar />
-            <Header></Header>
-
-           
-            <div className="">
-                <Footer />
+        <div className="relative w-full min-h-screen overflow-hidden">
+            {/* Particle Background */}
+            <div className="absolute inset-0 -z-10 ">
+                <Particles
+                    particleColors={['#ffffff', '#ffffff']}
+                    particleCount={200}
+                    particleSpread={10}
+                    speed={0.1}
+                    particleBaseSize={300}
+                    moveParticlesOnHover={true}
+                    alphaParticles={false}
+                    disableRotation={false}
+                />
             </div>
+
+            {/* Page Content */}
+            {/* <Background /> */}
+            <Navbar />
+            <Header />
+            <Footer />
         </div>
     );
 };
 
 export default HomeLayout;
+
